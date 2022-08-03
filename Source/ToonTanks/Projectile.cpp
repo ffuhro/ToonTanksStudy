acf5db0a2ef64cd0c7ef05d7aaac73d20a700006
Francisco Fuhro <ffuhro@outlook.com>
2022-08-03 04:07:36 -0300

    created projectile class Created the blueprint based on the projectile class Setted the projectile mesh at blueprint level

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -9,6 +9,9 @@ AProjectile::AProjectile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+    BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	RootComponent = BaseMesh;
+	
 }
 
 // Called when the game starts or when spawned
--- Source/ToonTanks/Projectile.h
+++ Source/ToonTanks/Projectile.h
@@ -19,6 +19,11 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess))	
+	UStaticMeshComponent* BaseMesh;
+
+	
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
